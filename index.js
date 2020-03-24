807b0b9fc36bf13f8a1c57f86dd1413e4dbf2ed6
John Loberger <johnloberger@gmail.com>
2020-03-24 22:17:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ function nowServing(katzDeliLine){
   else
   return "There is nobody waiting to be served!";
 }
-function currentLine(katzDeli) {
-  for (let i = 0; i < katzDeli.length; i++) {
-    katzDeli.push(` `+[i+1]+`. `  + katzDeli[i])
-  }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + katzDeli);
-}
+function currentLine(line) {
+   var lineNum = [];
+   if (line.length == 0) {
+     return "The line is currently empty.";
+   }
+   for (var i = 0; i < line.length; i++) {
+     lineNum.push(` ${i+1}. ${line[i]}`);
+ 	}
+   return "The line is currently:" + lineNum;
+ }
