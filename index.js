545c953e61836a3b6640bef90a27c83d6db23c52
John Loberger <johnloberger@gmail.com>
2020-03-24 22:18:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ function nowServing(katzDeliLine){
   return "There is nobody waiting to be served!";
 }
 function currentLine(line) {
-   var lineNum = [];
    if (line.length == 0) {
      return "The line is currently empty.";
    }
    for (var i = 0; i < line.length; i++) {
-     lineNum.push(` ${i+1}. ${line[i]}`);
+     katzDeli.push(` ${i+1}. ${line[i]}`);
  	}
-   return "The line is currently:" + lineNum;
+   return "The line is currently:" + katzDeli;
  }
