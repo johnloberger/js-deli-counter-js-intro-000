6dd0810090a92f7106e0f9014721e9ea9ba1ca2d
John Loberger <johnloberger@gmail.com>
2020-03-24 22:13:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ function takeANumber(katzDeli, name){
 }
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift()}.`;
-  else return "There is nobody waiting to be served!";
+  else 
+  return "There is nobody waiting to be served!";
+}
+var line = [];
+function currentLine(katzDeli) {
+  for (let i = 0; i < katzDeli.length; i++) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
 }
