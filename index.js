6d95caa7746e783c837d8ecd21ffb5fcf5bbe90d
John Loberger <johnloberger@gmail.com>
2020-03-24 21:53:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function takeANumber(katzDeli, name){
   katzDeli.push(`${name}`)
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
 }
+function nowServing(katzDeliLine){
+  let i = 0
+  while (i < katzDeli.length){
+    i++
+  }
+  if (katzDeli.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}`)
+  }
